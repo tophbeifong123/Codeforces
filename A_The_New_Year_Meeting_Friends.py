4f02230d9981e38729a977155d27810c5b7d423c
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2025-07-21 01:17:05 +0700

    add 723A

---
+++ A_The_New_Year_Meeting_Friends.py
@@ -0,0 +1,13 @@
+x1,x2,x3 = [int(x) for x in input().split()]
+point = 0
+
+if x1 > x2 and x1 < x3 or x1 < x2 and x1 > x3:
+    point = x1
+elif x2 > x1 and x2 < x3 or x2 < x1 and x2 > x3:
+    point = x2
+else:
+    point = x3
+
+print(point)
+
+print(int(abs(x1 - point) + abs(x2 - point) + abs(x3 - point)))
