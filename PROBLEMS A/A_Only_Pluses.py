b6254abc627cb59f415da05945cb8f1b9ffb9b62
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-07-20 17:02:11 +0700

    add A_Only_Pluses

--- PROBLEMS A/A_Only_Pluses.py
+++ PROBLEMS A/A_Only_Pluses.py
@@ -4,9 +4,10 @@ for _ in range(int(input())):
     for i in range(5):
         if a <= b  and a <=c:
             a += 1
-        elif b <=a and b <=a:
+        elif b <=a and b <=c:
             b += 1
         elif c <=a and c <=b:
             c += 1
 
     print(a*b*c)
+
