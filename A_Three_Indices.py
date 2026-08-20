c4e1569f549ea9cb3afc22d4e90f532c0236991c
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-08-20 18:24:07 +0700

    A_Three_Indices

--- A_Three_Indices.py
+++ A_Three_Indices.py
@@ -1,5 +1,16 @@
-for _ in range(int(input())):
+for i in range(int(input())):
     n = int(input())
     p = [int(x) for x in input().split()]
 
-    
+    ok = False
+
+    for j in range(1,n-1):
+        if p[j-1] < p[j] and p[j] > p[j+1]:
+            print('YES')
+            print(j ,j + 1,j + 2)
+            ok = True
+            break
+
+
+    if not ok:
+        print('NO')
