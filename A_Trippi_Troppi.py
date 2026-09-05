d87913c57e56271f7d190d8a27d95703663301eb
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-09-05 18:08:37 +0700

    B_The_67_th_6_7_Interger

---
+++ A_Trippi_Troppi.py
@@ -0,0 +1,4 @@
+for _ in range(int(input())):
+    s = input().split()
+
+    print(s[0][0]+s[1][0]+s[2][0])
--- B_The_67_th_6_7_Integer_Problem.py
+++ B_The_67_th_6_7_Integer_Problem.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
-    a = sorted([int(x) for x in input().split()])
-    print(-sum(a[0:-1])+a[-1])
+    a  = [int(x) for x in input().split()]
+    print(-1*sum(a[0:-2]) + a[1])
